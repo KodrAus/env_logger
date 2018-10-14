b0e80019412959c6aa385bd18fbfa0098a3f27ea
Ashley Mannix <ashleymannix@live.com.au>
2018-10-14 11:50:14 +1000

    update structured example

--- examples/structured.rs
+++ examples/structured.rs
@@ -20,13 +20,17 @@ extern crate log;
 extern crate env_logger;
 
 fn main() {
+    use std::fmt::Debug;
+
     env_logger::init_from_env("MY_LOG_LEVEL");
 
-    log!(log::Level::Info, msg: { "A structured {name}", name = "log" }, kvs: {
-        id: 1,
-        #[log(display)]
-        name: "log",
-        #[log(path)]
-        path: "./monkey-path",
+    let correlation_id = 123;
+    let user = "some user";
+
+    log!(log::Level::Info, msg: { "This is the rendered {message}. It is not structured", message = "message" }, kvs: {
+        #[log(fmt = Debug::fmt)]
+        correlation: correlation_id,
+        #[log(serde)]
+        user: user,
     });
 }
